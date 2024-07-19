1c10f46eca2d3ea035da58ceec85f7bcda85a6cf
lusanyadav <lusanyadav7@gmail.com>
2024-07-19 21:27:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import math
 pygame.init()
 
 
-class DrawInformation:
-    BLACK = 0, 0, 0
-    WHITE = 255, 255, 255
-    GREEN = 0, 255, 0
-    RED = 255, 0, 0
+class 
     BACKGROUND_COLOR = WHITE
 
     GRADIENTS = [
